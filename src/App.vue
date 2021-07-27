91c5a81a67fe92b69c1f110276424da5392cc81a
diogeneshas <diogenes.henrique.br@gmail.com>
2021-07-27 19:50:13 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <LearningResources v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link" />
+    </ul>
+</template>
+
+<script>
+import LearningResources from './components/learning-resources/LearningResources.vue'
+export default {
+    components: {
+        LearningResources
+    },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.org'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
