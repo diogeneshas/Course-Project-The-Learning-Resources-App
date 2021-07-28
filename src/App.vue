445b22a22c9d5281169e70f08b9c97ff217c380c
diogeneshas <diogenes.henrique.br@gmail.com>
2021-07-28 00:54:03 -0300

    Styling and More Components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-    <ul>
-        <LearningResources v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.description" :link="res.link" />
-    </ul>
+   <StoredResources :resources="storedResources" />
 </template>
 
 <script>
-import LearningResources from './components/learning-resources/LearningResources.vue'
+import StoredResources from './components/learning-resources/StoredResources.vue'
 export default {
     components: {
-        LearningResources
+        StoredResources
     },
     data() {
         return {
@@ -32,5 +30,17 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
 </style>
